0915bed74f734bff9b066008269c3b15b65841b0
7006 <alexbox86@gmail.com>
2025-11-23 10:47:28 +0200

    _5 sol

--- python/challenge_puzzles/starter_code/_5.py
+++ python/challenge_puzzles/starter_code/_5.py
@@ -1,2 +1,6 @@
 def get_longest_string(input_strs: list[str]) -> str:
-    pass
+    longest_str = ""
+    for input_str in input_strs:
+        if len(input_str) > len(longest_str):
+            longest_str = input_str
+    return longest_str

0bd8a10838d34354518ff05a9636049987ddc24d
7006 <alexbox86@gmail.com>
2025-11-23 11:22:13 +0200

    p5 sol

--- erlang/src/p5.erl
+++ erlang/src/p5.erl
@@ -1,5 +1,21 @@
 -module(p5).
--export([get_longest/1]).
+-export([get_longest_str/1]).
+-export([get_longest_binary_str/1]).
 
-get_longest(InputStrs) ->
-    "".
+get_longest_str(InputStrs) ->
+    Fn = fun
+        (Input, Longest) when length(Input) > length(Longest) ->
+            Input;
+        (_Input, Longest) ->
+            Longest
+    end,
+    lists:foldl(Fn, "", InputStrs).
+
+get_longest_binary_str(InputStrs) ->
+    Fn = fun
+        (Input, Longest) when byte_size(Input) > byte_size(Longest) ->
+            Input;
+        (_Input, Longest) ->
+            Longest
+    end,
+    lists:foldl(Fn, <<"">>, InputStrs).
--- erlang/test/p5_test.erl
+++ erlang/test/p5_test.erl
@@ -5,15 +5,15 @@ get_longest_string_test_() ->
     [
         ?_assertEqual(
             "lizard",
-            p5:get_longest(["cat", "dog", "bird", "lizard"])
+            p5:get_longest_str(["cat", "dog", "bird", "lizard"])
         ),
         ?_assertEqual(
             "bird",
-            p5:get_longest(["cat", "dog", "bird", "wolf"])
+            p5:get_longest_str(["cat", "dog", "bird", "wolf"])
         ),
         ?_assertEqual(
             "a",
-            p5:get_longest(["a", "b", "c", "d"])
+            p5:get_longest_str(["a", "b", "c", "d"])
         )
     ].
 
@@ -21,14 +21,14 @@ get_longest_binary_string_test_() ->
     [
         ?_assertEqual(
             <<"lizard">>,
-            p5:get_longest([<<"cat">>, <<"dog">>, <<"bird">>, <<"lizard">>])
+            p5:get_longest_binary_str([<<"cat">>, <<"dog">>, <<"bird">>, <<"lizard">>])
         ),
         ?_assertEqual(
             <<"bird">>,
-            p5:get_longest([<<"cat">>, <<"dog">>, <<"bird">>, <<"wolf">>])
+            p5:get_longest_binary_str([<<"cat">>, <<"dog">>, <<"bird">>, <<"wolf">>])
         ),
         ?_assertEqual(
             <<"a">>,
-            p5:get_longest([<<"a">>, <<"b">>, <<"c">>, <<"d">>])
+            p5:get_longest_binary_str([<<"a">>, <<"b">>, <<"c">>, <<"d">>])
         )
     ].

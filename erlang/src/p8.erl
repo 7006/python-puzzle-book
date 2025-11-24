9b2b5d337e8b06495ce70cdf2c3b8cfe6542e70b
7006 <alexbox86@gmail.com>
2025-11-25 00:26:01 +0200

    8 sol

--- erlang/src/p8.erl
+++ erlang/src/p8.erl
@@ -2,4 +2,4 @@
 -export([filter_type_str/1]).
 
 filter_type_str(InputList) ->
-    [ListItem || ListItem <- InputList].
+    [ListItem || ListItem <- InputList, is_list(ListItem) or is_binary(ListItem)].
--- erlang/test/p8_test.erl
+++ erlang/test/p8_test.erl
@@ -7,3 +7,10 @@ filter_type_str_test_() ->
         ?_assertEqual([], p8:filter_type_str([])),
         ?_assertEqual([], p8:filter_type_str([1, 2, 3, 4, 5]))
     ].
+
+filter_type_binary_str_test_() ->
+    [
+        ?_assertEqual([<<"hello">>, <<"www">>], p8:filter_type_str([<<"hello">>, 1, 2, <<"www">>])),
+        ?_assertEqual([], p8:filter_type_str([])),
+        ?_assertEqual([], p8:filter_type_str([1, 2, 3, 4, 5]))
+    ].
